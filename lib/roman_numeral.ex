7f373435e222e1493ab6d754b83ff88df21ea1f2
Jessie Morris <jmorris2@pillartechnology.com>
2019-02-07 19:17:55 -0500

    Simple solution to roman numeral kata

--- lib/roman_numeral.ex
+++ lib/roman_numeral.ex
@@ -3,10 +3,32 @@ defmodule RomanNumeral do
   Documentation for RomanNumeral.
   """
 
+  # def convert(number) do
+  #   {_, numeral} =
+  #     case number do
+  #       10 -> {10, "X" <> convert(number - 10)}
+  #       5 -> {5, "V"}
+  #       4 -> {4, "IV"}
+  #       2 -> {2, "I" <> convert(number - 1)}
+  #       1 -> {1, "I"}
+  #       _ -> {0, ""}
+  #     end
+  #   numeral
+  # end
   def convert(number) do
-    case number do
-      5 -> "V"
-      1 -> "I"
+    cond do
+      number >= 1000 -> "M" <> convert(number - 1000)
+      number >= 900 -> "CM" <> convert(number - 900)
+      number >= 100 -> "C" <> convert(number - 100)
+      number >= 90 -> "XC" <> convert(number - 90)
+      number >= 50 -> "L" <> convert(number - 50)
+      number >= 40 -> "XL" <> convert(number - 40)
+      number >= 10 -> "X" <> convert(number - 10)
+      number == 9 -> "IX" <> convert(number - 9)
+      number >= 5 -> "V" <> convert(number - 5)
+      number == 4 -> "IV" <> convert(number - 4)
+      number <= 3 && number > 0 -> "I" <> convert(number - 1)
+      number == 0 -> ""
     end
   end
 end
--- test/roman_numeral_test.exs
+++ test/roman_numeral_test.exs
@@ -4,7 +4,49 @@ defmodule RomanNumeralTest do
   test "when provided 1, returns I" do
     assert RomanNumeral.convert(1) == "I"
   end
+  test "when provided 2 returns II" do
+    assert RomanNumeral.convert(2) == "II"
+  end
+  test "when provided 4 returns IV" do
+    assert RomanNumeral.convert(4) == "IV"
+  end
   test "when provided 5 returns V" do
     assert RomanNumeral.convert(5) == "V"
+  end
+  test "when provided 9 returns IX" do 
+    assert RomanNumeral.convert(9) == "IX"
+  end
+  test "when provided 10 returns X" do
+    assert RomanNumeral.convert(10) == "X"
+  end
+  test "when provided 20 returns XX" do
+    assert RomanNumeral.convert(20) == "XX"
+  end
+  test "when provided 30 returns XXX" do
+    assert RomanNumeral.convert(30) == "XXX"
+  end
+  test "when provided 40 returns XL" do
+    assert RomanNumeral.convert(40) == "XL"
+  end
+  test "when provided 49 returns XLIX" do
+    assert RomanNumeral.convert(49) == "XLIX"
+  end
+  test "when provided 50 returns L" do
+    assert RomanNumeral.convert(50) == "L"
+  end
+  test "when provided 99 returns XCIX" do
+    assert RomanNumeral.convert(99) == "XCIX"
+  end
+  test "when provided 100 returns C" do
+    assert RomanNumeral.convert(100) == "C"
+  end
+  test "when provided 101 returns CI" do
+    assert RomanNumeral.convert(101) == "CI"
+  end
+  test "when provided 1000 returns M" do
+    assert RomanNumeral.convert(1000) == "M"
+  end
+  test "when provided 1999 returns MCMXCIX" do
+    assert RomanNumeral.convert(1999) == "MCMXCIX"
   end
 end
