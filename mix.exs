db556dee00d4c95a4ad15b45d1310adf3ffe1421
Jessie Morris <jmorris2@pillartechnology.com>
2019-02-07 19:20:24 -0500

    adding coveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule RomanNumeral.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       deps: deps()
     ]
   end
@@ -23,6 +25,7 @@ defmodule RomanNumeral.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.10", only: :test}, 
     ]
   end
 end
