2b45f18ae214b13c64a6d83b3e13d1aa110432eb
Jessie Morris <jmorris2@pillartechnology.com>
2019-02-07 21:39:43 -0500

    Two working implementations albiet sloppy tests

--- lib/roman_numeral.ex
+++ lib/roman_numeral.ex
@@ -3,19 +3,39 @@ defmodule RomanNumeral do
   Documentation for RomanNumeral.
   """
 
-  def convert(number) do
+  def symbol_map() do
+    [{1000, "M"},{900, "CM"},{500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}]
+  end
+
+  def next_symbol(number) do
+    Enum.filter(symbol_map(), fn {val, numeral} -> val <= number end)
+    |> List.first()
+  end
+
+  def convert(number, numeral \\ "") do
+    {value, symbol} = next_symbol(number)
+    if(number - value > 0) do
+       convert(number - value, numeral <> symbol)
+    else
+      numeral <> symbol
+    end
+  end
+
+  def convert2(number) do
     cond do
-      number >= 1000 -> "M" <> convert(number - 1000)
-      number >= 900 -> "CM" <> convert(number - 900)
-      number >= 100 -> "C" <> convert(number - 100)
-      number >= 90 -> "XC" <> convert(number - 90)
-      number >= 50 -> "L" <> convert(number - 50)
-      number >= 40 -> "XL" <> convert(number - 40)
-      number >= 10 -> "X" <> convert(number - 10)
-      number == 9 -> "IX" <> convert(number - 9)
-      number >= 5 -> "V" <> convert(number - 5)
-      number == 4 -> "IV" <> convert(number - 4)
-      number <= 3 && number > 0 -> "I" <> convert(number - 1)
+      number >= 1000 -> "M" <> convert2(number - 1000)
+      number >= 900 -> "CM" <> convert2(number - 900)
+      number >= 500 -> "D" <> convert2(number - 500)
+      number >= 400 -> "CD" <> convert2(number - 400)
+      number >= 100 -> "C" <> convert2(number - 100)
+      number >= 90 -> "XC" <> convert2(number - 90)
+      number >= 50 -> "L" <> convert2(number - 50)
+      number >= 40 -> "XL" <> convert2(number - 40)
+      number >= 10 -> "X" <> convert2(number - 10)
+      number == 9 -> "IX" <> convert2(number - 9)
+      number >= 5 -> "V" <> convert2(number - 5)
+      number == 4 -> "IV" <> convert2(number - 4)
+      number <= 3 && number > 0 -> "I" <> convert2(number - 1)
       number == 0 -> ""
     end
   end
--- test/roman_numeral_test.exs
+++ test/roman_numeral_test.exs
@@ -43,10 +43,72 @@ defmodule RomanNumeralTest do
   test "when provided 101 returns CI" do
     assert RomanNumeral.convert(101) == "CI"
   end
+  test "when provided 400 returns cd" do
+    assert RomanNumeral.convert(400) == "CD"
+  end
+  test "when provided 500 returns D" do
+    assert RomanNumeral.convert(500) == "D"
+  end
   test "when provided 1000 returns M" do
     assert RomanNumeral.convert(1000) == "M"
   end
   test "when provided 1999 returns MCMXCIX" do
     assert RomanNumeral.convert(1999) == "MCMXCIX"
+  end
+
+
+  test "when2 provided 1, returns I" do
+    assert RomanNumeral.convert2(1) == "I"
+  end
+  test "when2 provided 2 returns II" do
+    assert RomanNumeral.convert2(2) == "II"
+  end
+  test "when2 provided 4 returns IV" do
+    assert RomanNumeral.convert2(4) == "IV"
+  end
+  test "when2 provided 5 returns V" do
+    assert RomanNumeral.convert2(5) == "V"
+  end
+  test "when2 provided 9 returns IX" do 
+    assert RomanNumeral.convert2(9) == "IX"
+  end
+  test "when2 provided 10 returns X" do
+    assert RomanNumeral.convert2(10) == "X"
+  end
+  test "when2 provided 20 returns XX" do
+    assert RomanNumeral.convert2(20) == "XX"
+  end
+  test "when2 provided 30 returns XXX" do
+    assert RomanNumeral.convert2(30) == "XXX"
+  end
+  test "when2 provided 40 returns XL" do
+    assert RomanNumeral.convert2(40) == "XL"
+  end
+  test "when2 provided 49 returns XLIX" do
+    assert RomanNumeral.convert2(49) == "XLIX"
+  end
+  test "when2 provided 50 returns L" do
+    assert RomanNumeral.convert2(50) == "L"
+  end
+  test "when2 provided 99 returns XCIX" do
+    assert RomanNumeral.convert2(99) == "XCIX"
+  end
+  test "when2 provided 100 returns C" do
+    assert RomanNumeral.convert2(100) == "C"
+  end
+  test "when2 provided 101 returns CI" do
+    assert RomanNumeral.convert2(101) == "CI"
+  end
+  test "when2 provided 400 returns cd" do
+    assert RomanNumeral.convert2(400) == "CD"
+  end
+  test "when2 provided 500 returns D" do
+    assert RomanNumeral.convert2(500) == "D"
+  end
+  test "when2 provided 1000 returns M" do
+    assert RomanNumeral.convert2(1000) == "M"
+  end
+  test "when2 provided 1999 returns MCMXCIX" do
+    assert RomanNumeral.convert2(1999) == "MCMXCIX"
   end
 end
